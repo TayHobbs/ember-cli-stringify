06458f1670e5787a882ea0644d7a0c8f887c5a35
Kyle Hornberg <kyle.hornberg@gmail.com>
2016-10-05 10:26:53 -0500

    Test: Show that stringified objects can be returned to their native structures and used accordingly

--- tests/dummy/app/controllers/main.js
+++ tests/dummy/app/controllers/main.js
@@ -6,6 +6,14 @@ export default Ember.Controller.extend({
 
   env: Ember.computed(function() {
     return ENV;
+  }),
+
+  user: Ember.computed(function() {
+    return JSON.parse(ENV.json.user);
+  }),
+
+  comment: Ember.computed(function() {
+    return Ember.String.htmlSafe(ENV.templates.comment);
   })
 
 });
