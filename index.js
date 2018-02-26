ad634869eeb2ab2dc4127513b51c957f3cd3e009
TayHobbs <hobbstay@gmail.com>
2018-02-26 09:40:03 -0600

    Bug Fix: Fix index.js for ember 2.18

--- index.js
+++ index.js
@@ -4,13 +4,12 @@
 var fs = require('fs');
 var path = require('path');
 
-var stringifiedFiles = {};
-
 module.exports = {
   name: 'ember-cli-stringify',
 
-  preBuild: function() {
-    var app_config = this.app.project.config(this.app.env)['stringifyDirs'] || [];
+  config: function(_, config) {
+    let stringifiedFiles = {};
+    var app_config = config['stringifyDirs'] || [];
     for (var d = 0; d < app_config.length; d++) {
       var dir = app_config[d];
       var name = dir.split('/')[dir.split('/').length - 1];
@@ -21,11 +20,7 @@ module.exports = {
       for (var i = 0;  i < templates.length; i++) {
         stringifiedFiles[name][templates[i].replace(/\.[^.]+$/g, '')] = fs.readFileSync(path.join(process.cwd(), dir, templates[i])).toString().replace(/(?:\r\n|\r|\n)/g, '');
       }
-
     }
-  },
-
-  config: function() {
     return stringifiedFiles;
   }
 };
